324584e4f0306eeec709ba697c0480ae0ded1ae1
Stephen Condon <scondon87@gmail.com>
2026-01-02 07:12:34 -0600

    completed day 3

---
+++ 3/go.mod
@@ -0,0 +1,5 @@
+module day3
+
+go 1.25.0
+
+require github.com/stephen-condon/advent-of-code-2025/utilities v0.0.0-20260102034909-835d6054974b
---
+++ 3/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/stephen-condon/advent-of-code-2025/utilities"
+)
+
+func main() {
+	fmt.Println("Part One:")
+	executePartOne("example.txt")
+	executePartOne("input.txt")
+
+	fmt.Println("\nPart Two:")
+	executePartTwo("example.txt")
+	executePartTwo("input.txt")
+}
+
+func executePartOne(filename string) {
+	input := utilities.LoadInput(filename)
+	if len(input) == 0 {
+		fmt.Println("No input found")
+		return
+	}
+
+	totalSum := 0
+
+	for _, line := range input {
+		maxNumber := findLargestTwoDigitNumber(line)
+		totalSum += maxNumber
+	}
+
+	fmt.Printf("%s: %d\n", filename, totalSum)
+}
+
+// implement part two:
+// the difference in part two is tht instead of using 2 digits, we will be using 12
+// do not modify any code used in part one
+
+func executePartTwo(filename string) {
+	input := utilities.LoadInput(filename)
+	if len(input) == 0 {
+		fmt.Println("No input found")
+		return
+	}
+
+	totalSum := 0
+
+	for _, line := range input {
+		maxNumber := findLargestTwelveDigitNumber(line)
+		totalSum += maxNumber
+	}
+
+	fmt.Printf("%s: %d\n", filename, totalSum)
+}
+
+func findLargestTwelveDigitNumber(line string) int {
+	var digits []int
+	for i := 0; i < len(line); i++ {
+		if line[i] >= '0' && line[i] <= '9' {
+			digits = append(digits, int(line[i]-'0'))
+		}
+	}
+
+	if len(digits) < 12 {
+		return 0
+	}
+
+	result := make([]int, 12)
+	startPos := 0
+
+	for pos := 0; pos < 12; pos++ {
+		digitsNeeded := 12 - pos
+		maxDigit := -1
+		maxDigitPos := -1
+
+		searchEnd := len(digits) - digitsNeeded + 1
+		for i := startPos; i < searchEnd; i++ {
+			if digits[i] > maxDigit {
+				maxDigit = digits[i]
+				maxDigitPos = i
+			}
+
+			if maxDigit == 9 {
+				break
+			}
+		}
+
+		result[pos] = maxDigit
+		startPos = maxDigitPos + 1
+	}
+	number := 0
+	for i := 0; i < 12; i++ {
+		number = number*10 + result[i]
+	}
+
+	return number
+}
+
+func findLargestTwoDigitNumber(line string) int {
+	maxNumber := 0
+
+	for i := 0; i < len(line); i++ {
+		if line[i] < '0' || line[i] > '9' {
+			continue
+		}
+
+		for j := i + 1; j < len(line); j++ {
+			if line[j] < '0' || line[j] > '9' {
+				continue
+			}
+
+			firstDigit := int(line[i] - '0')
+			secondDigit := int(line[j] - '0')
+			number := firstDigit*10 + secondDigit
+
+			if number > maxNumber {
+				maxNumber = number
+			}
+		}
+	}
+
+	return maxNumber
+}
